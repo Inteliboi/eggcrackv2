627cf2f3fc311a81ef5a23a60efd7c3f48639da9
ProperVortex <32077055+ProperVortex@users.noreply.github.com>
2018-05-25 22:08:07 -0400

    Add files via upload

---
+++ eggcrackv2.bat
@@ -0,0 +1,12 @@
+@echo off
+color 2
+echo ---------------------------------------------
+echo            EGGCRACK V2 Pro Edition
+echo ---------------------------------------------
+echo >nul
+:accountcheck
+set /p account="Enter account in email:password format: "
+echo Checking account %account%
+echo ACCOUNT VALID!!!
+pause
+goto accountcheck
